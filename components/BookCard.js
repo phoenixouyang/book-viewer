39498aa47a769447913c5d1b0b7e69c70645a7e9
unknown <ouyangphoenix@gmail.com>
2026-03-10 23:15:59 -0400

    adds favourites page

--- components/BookCard.js
+++ components/BookCard.js
@@ -1,5 +1,34 @@
-export default function BookCard() {
-    return (<>
-    
-    </>)
+import Error from "next/error";
+import Link from "next/link";
+import { Card } from "react-bootstrap";
+import useSWR from "swr"
+
+export default function BookCard({ workId }) {
+    const { data, error, isLoading } = useSWR(`https://openlibrary.org/works/${workId}.json`);
+
+    if (isLoading) {
+        return null
+    } else if (error || !data) {
+        return <Error statusCode={404} />
+    } else {
+        return (<>
+            <Card style={{width: '18rem'}}>
+                <Card.Img 
+                    variant="top"
+                    onError={(event) => {
+                        event.target.onerror = null; // Remove the event handler to prevent infinite loop
+                        event.target.src =
+                        "https://placehold.co/400x600?text=Cover+Not+Available";
+                    }}
+                    src={`https://covers.openlibrary.org/b/id/${data?.covers?.[0]}-M.jpg`}
+                    alt="Cover Image"
+                />
+                <Card.Body>
+                    <Card.Title>{data.title || ""}</Card.Title>
+                    <Card.Text>{data.first_published_date || "N/A"}</Card.Text>
+                    <Link href={`/works/${workId}`}>View Book</Link>
+                </Card.Body>
+            </Card>
+        </>)
+    }
 }
---
+++ pages/favourites.js
@@ -0,0 +1,24 @@
+import BookCard from "@/components/BookCard";
+import PageHeader from "@/components/PageHeader";
+import { favouritesAtom } from "@/store";
+import { useAtom } from "jotai";
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function Favourites() {
+    const [favourites, setFavouritesList] = useAtom(favouritesAtom)
+
+    return(<>
+        <PageHeader text="Favourites" subtext="All your favourite books, in one place" />
+        <Container>
+            <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+                {favourites && (
+                    favourites.map((id, index) => (
+                        <Col key={index}>
+                            <BookCard workId={id} />
+                        </Col>
+                    ))
+                )}
+            </Row>
+        </Container>
+    </>)
+}
