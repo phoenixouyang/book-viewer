98ff5740d04cc605f8bcc4729e4a94977c9dd479
unknown <ouyangphoenix@gmail.com>
2026-03-10 21:31:12 -0400

    update styling to use bookswatch

--- components/MainNav.js
+++ components/MainNav.js
@@ -3,12 +3,12 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function MainNav() {
     return(<>
-        <Navbar expand="lg" className="fixed-top navbar-dark bg-dark">
+        <Navbar expand="lg" className="fixed-top navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
             <Container>
                 <Navbar.Brand>Phoenix Ouyang</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link as={Link} href="/">Books</Nav.Link>
-                    <Nav.Link as={Link} href="/about">About</Nav.Link>
+                    <Nav.Link class="nav-link" as={Link} href="/">Books</Nav.Link>
+                    <Nav.Link class="nav-link" as={Link} href="/about">About</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
